ee517f5d5ac57850149bfac66b55520f66a00d8a
absara428 <saraabidi428@gmail.com>
2022-07-07 23:56:41 +0100

    Header, styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <span>React Context API Tutorial</span>
+      <ul className="nav">
+        <li className="prod">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="prod1">
+          <Link to="/cart">Card</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
