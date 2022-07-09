4abc16a68ab5d3f8d49f108f926b3e9da4a56256
absara428 <saraabidi428@gmail.com>
2022-07-09 21:12:12 +0100

    cart UI completed, state management pending

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import { faker } from "@faker-js/faker";
 import SingleProduct from "./SingleProduct";
 
+faker.seed(111);
+
 const Home = () => {
   const productsArray = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
     image: faker.image.food(),
+
+    // image: faker.image.food(true),
   }));
 
   const [products] = useState(productsArray);
