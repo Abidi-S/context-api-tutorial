4e9c93b12a1e52d71a8b331e6c292e5863933100
absara428 <saraabidi428@gmail.com>
2022-07-11 23:59:20 +0100

    cart total not working

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleProduct from "./SingleProduct";
 
 const Cart = ({ cart, setCart }) => {
+  const [total, setTotal] = useState();
+
+  useEffect(() => {
+    setTotal(
+      cart.reduce((acc, curr) => acc + Number(curr.price)),
+      0
+    );
+  }, [cart]);
+
   return (
     <div>
       <span style={{ fontSize: 30 }}>My Cart</span>
       <br />
-      <span style={{ fontSize: 30 }}>Total: £10</span>
+      <span style={{ fontSize: 30 }}>Total: {total}</span>
       <div className="productContainer">
         {cart.map((prod) => (
           <SingleProduct
